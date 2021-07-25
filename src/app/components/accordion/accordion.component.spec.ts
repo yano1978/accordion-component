f6443236e21e1c8dfb5411ab7cd91528c3ab6d21
Ernesto Ianuario <ernestoianuario@Ernestos-iMac.local>
2021-07-25 15:03:44 +0100

    Removed useless test for app component + added Firefox karma test and dependency

--- src/app/components/accordion/accordion.component.spec.ts
+++ src/app/components/accordion/accordion.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AccordionComponent } from './accordion.component';
 
 describe('AccordionComponent', () => {
@@ -8,6 +8,7 @@ describe('AccordionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ], 
       declarations: [ AccordionComponent ]
     })
     .compileComponents();
